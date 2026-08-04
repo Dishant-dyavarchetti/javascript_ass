c379dc190c08b0a89ff0b270a7012510436a054d
rcc <rcc@SYSTEMA35.WINSR2.COM>
2026-08-04 17:34:01 +0530

    completed the assignment question 5

--- ass5.js
+++ ass5.js
@@ -8,7 +8,7 @@ function Student(roll, name, std, marks){
 let listStudents = [];
 
 function _createStudentObj(roll, name, std, marks){
-    temp = new Student(roll, name, std, marks);
+    let temp = new Student(roll, name, std, marks);
     if(temp){
         listStudents.push(temp);
         return true;
@@ -17,12 +17,12 @@ function _createStudentObj(roll, name, std, marks){
     }
 }
 
-function _readStudent(input, name){
+function _readStudent(input, name = null){
     // read all students
     if(input === "1"){
         console.log("---All Students Details---")
         listStudents.forEach((student) => {
-            console.log(`roll No: ${student.roll} \nstudent Name: ${student.name} \nStudent Grade: ${student.std} \nStudent Marks ${student.marks}`);
+            console.log(`roll No: ${student.roll} \nstudent Name: ${student.name} \nStudent Grade: ${student.std} \nStudent Marks ${student.marks} \n`);
         });
     }
     // read specific Student
@@ -40,21 +40,22 @@ function _readStudent(input, name){
 
 function _updateStudentDetails(roll, det){
     let newRoll = roll.trim().toLowerCase();
-    let upStud = listStudents.find(stud => stud.roll.trim().toLowerCase() === newRoll);
-    if(upStud){
-        upStud = det;
+    let upStud = listStudents.findIndex(stud => stud.roll.trim().toLowerCase() === newRoll);
+    if(upStud !== -1){
+        listStudents[upStud] = det;
         // console.log(`Student ${upStud.name} updated Successfully...`);
         return true;
     }else{
         return false;
     }
+    
 }
 
 function _removeStudent(roll){
     let rollNo = roll.trim().toLowerCase();
-    let student = listStudents.find(stud => stud.roll.trim().toLowerCase() === rollNo);
-    if(student){
-        delete student;
+    let student = listStudents.findIndex(stud => stud.roll.trim().toLowerCase() === rollNo);
+    if(student !== -1){
+        listStudents.splice(student, 1);
         return true;
     }else{
         return false;
@@ -70,15 +71,16 @@ let rl = readline.createInterface({
 });
 
 async function main(){
-    console.log("CRUD Operations for Student List...");
-    console.log("1) Create Student Object and add");
-    console.log("2) Read Student Details");
-    console.log("3) Update the details of the student");
-    console.log("4) Delete the data of the Student");
-    console.log("5) Exit");
-    let choice = await rl.question("Enter the Choice Between [1-5]: ");
+    
     let run = true;
     while(run){
+        console.log("CRUD Operations for Student List...");
+        console.log("1) Create Student Object and add");
+        console.log("2) Read Student Details");
+        console.log("3) Update the details of the student");
+        console.log("4) Delete the data of the Student");
+        console.log("5) Exit");
+        let choice = await rl.question("Enter the Choice Between [1-5]: ");
         switch(choice){
             case "1":
                 let roll = await rl.question("Enter the Roll No of the student: ");
@@ -98,9 +100,11 @@ async function main(){
                 console.log("1) All students");
                 console.log("2) Particular Student");
                 let cho2 = await rl.question("Enter the choice between [1-2]: ");
-                if(cho2){
+                if(cho2 === "2"){
                     let det = await rl.question("Enter the student name: ");
                     _readStudent(cho2, det);
+                }else{
+                    _readStudent(cho2);
                 }
                 break;
             case "3":
@@ -111,15 +115,36 @@ async function main(){
                     let grade = await rl.question("Enter the grade of the student: ");
                     let marks = await rl.question("Enter the marks of the student: ");
                     let upstud = new Student(roll, name, grade, marks);
-                    let res2 = _updateStudentDetails(roll, upstud);
+                    let res2 = _updateStudentDetails(cho3, upstud);
                     if(res2){
-                        console.log("Student is ")
+                        console.log("Student details are successfully updated");
+                    }else{
+                        console.log("Student Detailed are not updated");
                     }
                 }
+                break;
+            case "4":
+                let cho4 = await rl.question("ENTER THE ROLL NO OF THE STUDENT: ");
+                if(cho4){
+                    let res = _removeStudent(cho4);
+                    if(res){
+                        console.log(`Student removed successfully...`);
+                    }else{
+                        console.log(`Student didnt removed successfully...`);
+                    }
+                    
+                }
+                break;
+            case "5":
+                console.log("Exiting from program...");
+                run = false;
+                break;
+            default:
+                console.log("Enter correct input...");
                 break;
         }
     } 
     rl.close();
 };
 
-main()
+main();
